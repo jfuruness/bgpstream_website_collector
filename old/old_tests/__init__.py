3e2724d7152dea5782ddfdaef7511d26f09ba7f0
Justin Furuness <jfuruness@gmail.com>
2023-12-01 04:36:24 -0500

    working through pip install issues

--- old/old_tests/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-__authors__ = ["Justin Furuness"]
-__credits__ = ["Justin Furuness"]
-__Lisence__ = "BSD"
-__maintainer__ = "Justin Furuness"
-__email__ = "jfuruness@gmail.com"
-__status__ = "Development"
